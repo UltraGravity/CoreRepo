204d8632b211795aee2ec9be70b64de6b39206a0
Brandon Howard <Brandon Howard@192.168.28.130>
2014-10-06 17:33:08 -0500

    Extends Game instead of ApplicationAdapter

--- UltraGravity-core/src/com/APAAAEAIA/UltraGravity/MyGame.java
+++ UltraGravity-core/src/com/APAAAEAIA/UltraGravity/MyGame.java
@@ -1,12 +1,13 @@
 package com.APAAAEAIA.UltraGravity;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-public class MyGame extends ApplicationAdapter {
+public class MyGame extends Game {
 	SpriteBatch batch;
 	Texture img;
 	
