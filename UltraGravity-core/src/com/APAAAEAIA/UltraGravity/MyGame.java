268482d9825782db9382ce3ad2b95ac98c13ec83
Brandon Howard <Brandon Howard@192.168.28.130>
2014-10-06 18:01:16 -0500

    Set up myGame

--- UltraGravity-core/src/com/APAAAEAIA/UltraGravity/MyGame.java
+++ UltraGravity-core/src/com/APAAAEAIA/UltraGravity/MyGame.java
@@ -9,21 +9,10 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 public class MyGame extends Game 
 {
 	SpriteBatch batch;
-	Texture img;
 	
 	@Override
 	public void create () 
 	{
-
-	}
-
-	@Override
-	public void render () 
-	{
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+		// Create main game
 	}
 }
