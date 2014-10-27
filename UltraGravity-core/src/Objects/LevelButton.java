e45ee589c32b66495976184abf9af7e8590388a2
Brandon Howard <Brandon Howard>
2014-10-26 21:13:13 -0500

    Added a scrollable level select screen. The buttons don't do anything now. They are just buttons without any action handler.

---
+++ UltraGravity-core/src/Objects/LevelButton.java
@@ -0,0 +1,17 @@
+package Objects;
+
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+
+public class LevelButton extends TextButton
+{
+
+	
+	
+	public LevelButton(String text, TextButtonStyle style) 
+	{
+		super(text, style);
+	}
+	
+	
+
+}
--- UltraGravity-core/src/Screens/LevelScreen.java
+++ UltraGravity-core/src/Screens/LevelScreen.java
@@ -1,19 +1,17 @@
 package Screens;
 
+import Objects.LevelButton;
+
 import com.APAAAEAIA.UltraGravity.MyGame;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
-import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.badlogic.gdx.scenes.scene2d.ui.Label;
-import com.badlogic.gdx.scenes.scene2d.ui.Label.LabelStyle;
+import com.badlogic.gdx.scenes.scene2d.ui.ScrollPane;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
-import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton.TextButtonStyle;
-import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
 
 public class LevelScreen extends GenericScreen
@@ -23,13 +21,10 @@ public class LevelScreen extends GenericScreen
 	BitmapFont font;
 	Skin skin;
 	Table table;
+	Table window;
+	ScrollPane scroll;
 	TextureAtlas buttonAtlas;
 	TextButtonStyle textButtonStyle;
-	TextButton levelSelectButton;
-	TextButton levelEditorButton;
-	TextButton optionsButton;
-	Label ultraGravity;
-	LabelStyle ultraGravityFont;
 
 	public LevelScreen(MyGame myGame)
 	{
@@ -39,7 +34,7 @@ public class LevelScreen extends GenericScreen
 	
 	public void render(float delta) 
 	{	
-        Gdx.gl.glClearColor(0, 0, 0, 1);
+        Gdx.gl.glClearColor(1, 1, 1, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         
         stage.act();
@@ -47,9 +42,7 @@ public class LevelScreen extends GenericScreen
 		batch.begin();
 		
 			//table.debug();
-			//table.debugTable();
-			
-		
+			//table.debugAll();
 			stage.draw();
 			
 		batch.end();
@@ -61,34 +54,47 @@ public class LevelScreen extends GenericScreen
 		stage = new Stage(new ScreenViewport());
 		Gdx.input.setInputProcessor(stage);
 		table = new Table();
-		table.setFillParent(true);
+		window = new Table();
+		window.setFillParent(true);
+		
+		window.defaults().expand().top();
 		
 		font = myGame.assetLoader.font;
+		skin = new Skin();
+		buttonAtlas = myGame.assetLoader.mainMenuButtonAtlas;
+		skin.addRegions(buttonAtlas);
+		textButtonStyle = new TextButtonStyle();
+		textButtonStyle.font = font;
+		textButtonStyle.up = skin.getDrawable("Button");
+		textButtonStyle.down = skin.getDrawable("Button-Pressed");
 
-		ultraGravityFont = new LabelStyle();
-		ultraGravityFont.font = myGame.assetLoader.font;
-		ultraGravity = new Label("The Level Screen", ultraGravityFont);
+		for (int i = 0; i < 50; i++)
+		{
+			LevelButton button = new LevelButton(String.valueOf(i), textButtonStyle);
+			table.add(button).width(screenWidth/5).height(screenWidth/5);
+			
+			if (i % 3 == 2)
+			{
+				table.row();
+			}
+		}
 		
-		table.add(ultraGravity).fillX();
-		stage.addActor(table);
+		
+		scroll = new ScrollPane(table);
+		
+		window.add(scroll);
+		
+		stage.addActor(window);
 	}
 
 	public void hide() 
 	{
-		System.out.println("Disposing Main Menu Screen");
+		System.out.println("Disposing Level Select Screen");
 		this.dispose();
 	}
 	
 	public void dispose() 
 	{
-		/*
-		 * It is very important that everything created is disposed of properly when it is no longer needed. I find it best
-		 * to explicitly set everything equal to null so the garbage collector knows it can remove the stuff from memory.
-		 * 
-		 * Calling super.dispose() will get rid of the built in variables, but it is important that anything that is
-		 * uniquely created in this class be disposed.
-		 */
-
 		super.dispose();
 		stage.dispose();
 	}
