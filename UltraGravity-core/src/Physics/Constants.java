4f25cf2a124eacd59c89114bbf5008cca3c52e0e
0670howardb <Brandon@192.168.128.1>
2014-12-06 12:36:48 -0600

    Push

--- UltraGravity-core/src/Physics/Constants.java
+++ UltraGravity-core/src/Physics/Constants.java
@@ -1,5 +1,7 @@
 package Physics;
 
+import com.badlogic.gdx.Gdx;
+
 public class Constants
 {
 	public final static float TIME_STEP = 1/60f;
@@ -8,7 +10,7 @@ public class Constants
 	
 //	public final static int SIZE_SCALE = 8;
 	
-	public final static float SIZE_SCALE = (float) 1.95;		//meters for each grid block
+	public final static float SIZE_SCALE = (float) 1.95;	 	//meters for each grid block
 	public final static float GROUND_SCALE = 2;
 	public final static int OBJECT_SCALE = 2;
 	public final static int GRID_TO_WORLD = 2 * OBJECT_SCALE;
